3fcfd2e4dda9b6d307426dd98ef8497a0cc31fb3
Anu Mittal <anu22mittal@gmail.com>
2021-02-22 22:09:30 +0800

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Anu Mittal on 22/02/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
